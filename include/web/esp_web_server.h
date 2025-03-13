a20ff70b13e9dfff623391f5a16f591efcd34692
coolbox13 <hello@coolbox.com>
2025-03-13 13:25:24 +0100

    removed old header files

--- include/web/esp_web_server.h
+++
@@ -1,7 +0,0 @@
-#ifndef ESP_WEB_SERVER_H
-#define ESP_WEB_SERVER_H
-
-// Empty stub file to satisfy includes
-// Web server functionality is disabled via DISABLE_ESP_WEB_SERVER flag
-
-#endif // ESP_WEB_SERVER_H
